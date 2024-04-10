2cb65147da0949ff0b096e93eba1c83b5c0b3195
Carlo331 <davidcarlanton06@gmail.com>
2024-04-10 09:52:27 +0200

    bilde lagt til

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -6,7 +6,10 @@
     <div id="overskrift" class="flex w-1/3 justify-center border-2 border-red-900">
         <span id="pc komponenter overskrift" class="text-green-600 text-3xl">PC komponenter</span>
     </div>
-    <div id="pc bilde div" class="flex w-2/3 h-3/5 border-2 items-center justify-center border-red-900">
-        halla
+    <div id="pc bilde div" class="flex w-2/3 h-5/6 border-2 items-center justify-center border-red-900">
+        <img id="pc bilde" class="h-full" src='pc.png' alt="gaming pc">
+        <div id="knappe div" class="absolute inset-0 flex h-full border-2 border-red-900 w-2/3">
+
+        </div>
     </div>
 </div>
