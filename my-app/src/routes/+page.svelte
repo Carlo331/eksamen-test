5ffe1fe7fd82e6f356305dd03094a7b376eb9a3d
Carlo331 <davidcarlanton06@gmail.com>
2024-04-10 09:22:44 +0200

    layout simple

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -2,6 +2,11 @@
     import "../app.css";
 
 </script>
-<div id="main" class="flex items-center justify-center bg-slate-300 h-screen w-screen">
-    pikk
+<div id="main" class="flex items-center justify-between  bg-slate-300 h-screen w-screen">
+    <div id="overskrift" class="flex w-1/3 justify-center border-2 border-red-900">
+        <span id="pc komponenter overskrift" class="text-green-600 text-3xl">PC komponenter</span>
+    </div>
+    <div id="pc bilde div" class="flex w-2/3 h-3/5 border-2 items-center justify-center border-red-900">
+        halla
+    </div>
 </div>
